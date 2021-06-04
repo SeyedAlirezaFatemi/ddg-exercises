d792b410870308d25b858a5f4ad642eb26e9ef48
Alireza <alirezafa@rocketmail.com>
2021-06-04 22:57:59 +0430

    Reformat discrete-exterior-calculus.cpp.

--- core/src/discrete-exterior-calculus.cpp
+++ core/src/discrete-exterior-calculus.cpp
@@ -1,7 +1,8 @@
 // PLEASE READ:
 //
-// This file additional geometry routines for the VertexPositionGeometry class in Geometry Central. Because we are
-// "inside" the class, we no longer have to call
+// This file additional geometry routines for the VertexPositionGeometry class
+// in Geometry Central. Because we are "inside" the class, we no longer have to
+// call
 //
 //          geometry->inputVertexPositions[v], etc.
 //
@@ -21,9 +22,11 @@
 //
 //          mesh.vertices(), etc.
 //
-// Functions in this file can be called from other projects simply by using geometry->buildHodgeStar0Form(), etc. where
-// "geometry" is a pointer to a VertexPositionGeometry. This avoids having to declare a GeometryRoutines object in every
-// project, and also mimics the way that geometry routines are normally called in Geometry Central.
+// Functions in this file can be called from other projects simply by using
+// geometry->buildHodgeStar0Form(), etc. where "geometry" is a pointer to a
+// VertexPositionGeometry. This avoids having to declare a GeometryRoutines
+// object in every project, and also mimics the way that geometry routines are
+// normally called in Geometry Central.
 //
 // Other notes: In this file, you can use the constant pi by using PI.
 
@@ -32,67 +35,68 @@
 namespace geometrycentral {
 namespace surface {
 
-
 /*
  * Build Hodge operator on 0-forms.
  * By convention, the area of a vertex is 1.
  *
  * Input:
- * Returns: A sparse diagonal matrix representing the Hodge operator that can be applied to discrete 0-forms.
+ * Returns: A sparse diagonal matrix representing the Hodge operator that can be
+ * applied to discrete 0-forms.
  */
 SparseMatrix<double> VertexPositionGeometry::buildHodgeStar0Form() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
  * Build Hodge operator on 1-forms.
  *
  * Input:
- * Returns: A sparse diagonal matrix representing the Hodge operator that can be applied to discrete 1-forms.
+ * Returns: A sparse diagonal matrix representing the Hodge operator that can be
+ * applied to discrete 1-forms.
  */
 SparseMatrix<double> VertexPositionGeometry::buildHodgeStar1Form() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
  * Build Hodge operator on 2-forms.
  *
  * Input:
- * Returns: A sparse diagonal matrix representing the Hodge operator that can be applied to discrete 2-forms.
+ * Returns: A sparse diagonal matrix representing the Hodge operator that can be
+ * applied to discrete 2-forms.
  */
 SparseMatrix<double> VertexPositionGeometry::buildHodgeStar2Form() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
  * Build exterior derivative on 0-forms.
  *
  * Input:
- * Returns: A sparse matrix representing the exterior derivative that can be applied to discrete 0-forms.
+ * Returns: A sparse matrix representing the exterior derivative that can be
+ * applied to discrete 0-forms.
  */
-SparseMatrix<double> VertexPositionGeometry::buildExteriorDerivative0Form() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+SparseMatrix<double> VertexPositionGeometry::buildExteriorDerivative0Form()
+    const {
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
  * Build exterior derivative on 1-forms.
  *
  * Input:
- * Returns: A sparse matrix representing the exterior derivative that can be applied to discrete 1-forms.
+ * Returns: A sparse matrix representing the exterior derivative that can be
+ * applied to discrete 1-forms.
  */
-SparseMatrix<double> VertexPositionGeometry::buildExteriorDerivative1Form() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+SparseMatrix<double> VertexPositionGeometry::buildExteriorDerivative1Form()
+    const {
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
-} // namespace surface
-} // namespace geometrycentral
+}  // namespace surface
+}  // namespace geometrycentral
