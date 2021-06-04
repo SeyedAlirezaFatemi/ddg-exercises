7b590c689d44f5254e11e7de9d8b58afccf3f074
Alireza <alirezafa@rocketmail.com>
2021-06-04 22:08:37 +0430

    Reformat geometry.cpp.

--- core/src/geometry.cpp
+++ core/src/geometry.cpp
@@ -1,7 +1,8 @@
 // PLEASE READ:
 //
-// This file implements additional geometry routines for the VertexPositionGeometry class in Geometry Central. Because
-// we are "inside" the class, we no longer have to call
+// This file implements additional geometry routines for the
+// VertexPositionGeometry class in Geometry Central. Because we are "inside" the
+// class, we no longer have to call
 //
 //          geometry->inputVertexPositions[v], etc.
 //
@@ -21,16 +22,17 @@
 //
 //          mesh.vertices(), etc.
 //
-// Functions in this file can be called from other projects simply by using geometry->cotan(he),
-// geometry->barycentricDualArea(v), etc. where "geometry" is a pointer to a VertexPositionGeometry. This avoids having
-// to declare a GeometryRoutines object in every project, and also mimics the way that geometry routines are normally
-// called in Geometry Central.
+// Functions in this file can be called from other projects simply by using
+// geometry->cotan(he), geometry->barycentricDualArea(v), etc. where "geometry"
+// is a pointer to a VertexPositionGeometry. This avoids having to declare a
+// GeometryRoutines object in every project, and also mimics the way that
+// geometry routines are normally called in Geometry Central.
 //
 // Other notes: In this file, you can use the constant pi by using PI.
 
+#include <complex>
 
 #include "geometrycentral/surface/vertex_position_geometry.h"
-#include <complex>
 
 namespace geometrycentral {
 namespace surface {
@@ -39,7 +41,7 @@ namespace surface {
  * Compute the Euler characteristic of the mesh.
  */
 int VertexPositionGeometry::eulerCharacteristic() const {
-    return (int)mesh.nVertices() - (int)mesh.nEdges() + (int)mesh.nFaces();
+  return (int)mesh.nVertices() - (int)mesh.nEdges() + (int)mesh.nFaces();
 }
 
 /*
@@ -49,12 +51,11 @@ int VertexPositionGeometry::eulerCharacteristic() const {
  * Returns: The mean edge length.
  */
 double VertexPositionGeometry::meanEdgeLength() const {
-
-    double total = 0.0;
-    for (Edge e : mesh.edges()) {
-        total += edgeLength(e);
-    }
-    return total / mesh.nEdges();
+  double total = 0.0;
+  for (Edge e : mesh.edges()) {
+    total += edgeLength(e);
+  }
+  return total / mesh.nEdges();
 }
 
 /*
@@ -64,24 +65,23 @@ double VertexPositionGeometry::meanEdgeLength() const {
  * Returns: The surface area of the mesh.
  */
 double VertexPositionGeometry::totalArea() const {
-
-    double total = 0.0;
-    for (Face f : mesh.faces()) {
-        total += faceArea(f);
-    }
-    return total;
+  double total = 0.0;
+  for (Face f : mesh.faces()) {
+    total += faceArea(f);
+  }
+  return total;
 }
 
 /*
- * Computes the cotangent of the angle opposite to a halfedge. (Do NOT use built-in function for this)
+ * Computes the cotangent of the angle opposite to a halfedge. (Do NOT use
+ * built-in function for this)
  *
  * Input: The halfedge whose cotan weight is to be computed.
  * Returns: The cotan of the angle opposite the given halfedge.
  */
 double VertexPositionGeometry::cotan(Halfedge he) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
@@ -91,34 +91,33 @@ double VertexPositionGeometry::cotan(Halfedge he) const {
  * Returns: The barycentric dual area of the given vertex.
  */
 double VertexPositionGeometry::barycentricDualArea(Vertex v) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
- * Computes the angle (in radians) at a given corner. (Do NOT use built-in function for this)
+ * Computes the angle (in radians) at a given corner. (Do NOT use built-in
+ * function for this)
  *
  *
  * Input: The corner at which the angle needs to be computed.
  * Returns: The angle clamped between 0 and π.
  */
 double VertexPositionGeometry::angle(Corner c) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
- * Computes the signed angle (in radians) between two adjacent faces. (Do NOT use built-in function for this)
+ * Computes the signed angle (in radians) between two adjacent faces. (Do NOT
+ * use built-in function for this)
  *
- * Input: The halfedge (shared by the two adjacent faces) on which the dihedral angle is computed.
- * Returns: The dihedral angle.
+ * Input: The halfedge (shared by the two adjacent faces) on which the dihedral
+ * angle is computed. Returns: The dihedral angle.
  */
 double VertexPositionGeometry::dihedralAngle(Halfedge he) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
@@ -128,9 +127,8 @@ double VertexPositionGeometry::dihedralAngle(Halfedge he) const {
  * Returns: The "equally weighted" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalEquallyWeighted(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
@@ -140,9 +138,8 @@ Vector3 VertexPositionGeometry::vertexNormalEquallyWeighted(Vertex v) const {
  * Returns: The "tip angle weights" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalAngleWeighted(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
@@ -152,9 +149,8 @@ Vector3 VertexPositionGeometry::vertexNormalAngleWeighted(Vertex v) const {
  * Returns: The "inscribed sphere" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalSphereInscribed(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
@@ -164,9 +160,8 @@ Vector3 VertexPositionGeometry::vertexNormalSphereInscribed(Vertex v) const {
  * Returns: The "face area weighted" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalAreaWeighted(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
@@ -176,21 +171,20 @@ Vector3 VertexPositionGeometry::vertexNormalAreaWeighted(Vertex v) const {
  * Returns: The "Gauss curvature" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalGaussianCurvature(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
- * Computes the normal at a vertex using the "mean curvature" method (equivalent to the "area gradient" method).
+ * Computes the normal at a vertex using the "mean curvature" method (equivalent
+ * to the "area gradient" method).
  *
  * Input: The vertex on which the normal is to be computed.
  * Returns: The "mean curvature" normal vector.
  */
 Vector3 VertexPositionGeometry::vertexNormalMeanCurvature(Vertex v) const {
-
-    // TODO
-    return {0, 0, 0}; // placeholder
+  // TODO
+  return {0, 0, 0};  // placeholder
 }
 
 /*
@@ -200,9 +194,8 @@ Vector3 VertexPositionGeometry::vertexNormalMeanCurvature(Vertex v) const {
  * Returns: The angle defect of the given vertex.
  */
 double VertexPositionGeometry::angleDefect(Vertex v) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
@@ -212,9 +205,8 @@ double VertexPositionGeometry::angleDefect(Vertex v) const {
  * Returns: The total angle defect
  */
 double VertexPositionGeometry::totalAngleDefect() const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
@@ -224,9 +216,8 @@ double VertexPositionGeometry::totalAngleDefect() const {
  * Returns: The mean curvature at the given vertex.
  */
 double VertexPositionGeometry::scalarMeanCurvature(Vertex v) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
@@ -236,75 +227,75 @@ double VertexPositionGeometry::scalarMeanCurvature(Vertex v) const {
  * Returns: The circumcentric dual area of the given vertex.
  */
 double VertexPositionGeometry::circumcentricDualArea(Vertex v) const {
-
-    // TODO
-    return 0; // placeholder
+  // TODO
+  return 0;  // placeholder
 }
 
 /*
- * Computes the (pointwise) minimum and maximum principal curvature values at a vertex.
+ * Computes the (pointwise) minimum and maximum principal curvature values at a
+ * vertex.
  *
  * Input: The vertex on which the principal curvatures need to be computed.
- * Returns: A std::pair containing the minimum and maximum principal curvature values at a vertex.
+ * Returns: A std::pair containing the minimum and maximum principal curvature
+ * values at a vertex.
  */
-std::pair<double, double> VertexPositionGeometry::principalCurvatures(Vertex v) const {
-
-    // TODO
-    return std::make_pair(0, 0); // placeholder
+std::pair<double, double> VertexPositionGeometry::principalCurvatures(
+    Vertex v) const {
+  // TODO
+  return std::make_pair(0, 0);  // placeholder
 }
 
-
 /*
- * Builds the sparse POSITIVE DEFINITE Laplace matrix. Do this by building the negative semidefinite Laplace matrix,
- * multiplying by -1, and shifting the diagonal elements by a small constant (e.g. 1e-8).
+ * Builds the sparse POSITIVE DEFINITE Laplace matrix. Do this by building the
+ * negative semidefinite Laplace matrix, multiplying by -1, and shifting the
+ * diagonal elements by a small constant (e.g. 1e-8).
  *
  * Input:
  * Returns: Sparse positive definite Laplace matrix for the mesh.
  */
 SparseMatrix<double> VertexPositionGeometry::laplaceMatrix() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
- * Builds the sparse diagonal mass matrix containing the barycentric dual area of each vertex.
+ * Builds the sparse diagonal mass matrix containing the barycentric dual area
+ * of each vertex.
  *
  * Input:
  * Returns: Sparse mass matrix for the mesh.
  */
 SparseMatrix<double> VertexPositionGeometry::massMatrix() const {
-
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+  // TODO
+  return identityMatrix<double>(1);  // placeholder
 }
 
 /*
- * Builds the sparse complex POSITIVE DEFINITE Laplace matrix. Do this by building the negative semidefinite Laplace
- * matrix, multiplying by -1, and shifting the diagonal elements by a small constant (e.g. 1e-8).
+ * Builds the sparse complex POSITIVE DEFINITE Laplace matrix. Do this by
+ * building the negative semidefinite Laplace matrix, multiplying by -1, and
+ * shifting the diagonal elements by a small constant (e.g. 1e-8).
  *
  * Input:
  * Returns: Sparse complex positive definite Laplace matrix for the mesh.
  */
-SparseMatrix<std::complex<double>> VertexPositionGeometry::complexLaplaceMatrix() const {
-
-    // TODO
-    return identityMatrix<std::complex<double>>(1); // placeholder
+SparseMatrix<std::complex<double>>
+VertexPositionGeometry::complexLaplaceMatrix() const {
+  // TODO
+  return identityMatrix<std::complex<double>>(1);  // placeholder
 }
 
 /*
  * Compute the center of mass of a mesh.
  */
 Vector3 VertexPositionGeometry::centerOfMass() const {
+  // Compute center of mass.
+  Vector3 center = {0.0, 0.0, 0.0};
+  for (Vertex v : mesh.vertices()) {
+    center += inputVertexPositions[v];
+  }
+  center /= mesh.nVertices();
 
-    // Compute center of mass.
-    Vector3 center = {0.0, 0.0, 0.0};
-    for (Vertex v : mesh.vertices()) {
-        center += inputVertexPositions[v];
-    }
-    center /= mesh.nVertices();
-
-    return center;
+  return center;
 }
 
 /*
@@ -312,29 +303,28 @@ Vector3 VertexPositionGeometry::centerOfMass() const {
  * Also rescales the mesh to unit radius if <rescale> == true.
  */
 void VertexPositionGeometry::normalize(const Vector3& origin, bool rescale) {
+  // Compute center of mass.
+  Vector3 center = centerOfMass();
 
-    // Compute center of mass.
-    Vector3 center = centerOfMass();
+  // Translate to origin [of original mesh].
+  double radius = 0;
+  for (Vertex v : mesh.vertices()) {
+    inputVertexPositions[v] -= center;
+    radius = std::max(radius, inputVertexPositions[v].norm());
+  }
 
-    // Translate to origin [of original mesh].
-    double radius = 0;
+  // Rescale.
+  if (rescale) {
     for (Vertex v : mesh.vertices()) {
-        inputVertexPositions[v] -= center;
-        radius = std::max(radius, inputVertexPositions[v].norm());
+      inputVertexPositions[v] /= radius;
     }
+  }
 
-    // Rescale.
-    if (rescale) {
-        for (Vertex v : mesh.vertices()) {
-            inputVertexPositions[v] /= radius;
-        }
-    }
-
-    // Translate to origin [of original mesh].
-    for (Vertex v : mesh.vertices()) {
-        inputVertexPositions[v] += origin;
-    }
+  // Translate to origin [of original mesh].
+  for (Vertex v : mesh.vertices()) {
+    inputVertexPositions[v] += origin;
+  }
 }
 
-} // namespace surface
-} // namespace geometrycentral
+}  // namespace surface
+}  // namespace geometrycentral
