ef1cfa837e8f3f97272bd4cf86a39041e0f99b2e
Alireza <alirezafa@rocketmail.com>
2021-06-25 18:03:29 +0430

    Reformat + Implemented the constructor.

--- projects/poisson-problem/src/scalar-poisson-problem.cpp
+++ projects/poisson-problem/src/scalar-poisson-problem.cpp
@@ -4,27 +4,25 @@
 /* Constructor
  * Input: The surface mesh <inputMesh> and geometry <inputGeo>.
  */
-ScalarPoissonProblem::ScalarPoissonProblem(ManifoldSurfaceMesh* inputMesh, VertexPositionGeometry* inputGeo) {
-
-    mesh = inputMesh;
-    geometry = inputGeo;
-
-    // TODO: Build member variables A (Laplace matrix), M (mass matrix), total area
-    this->A = geometry->laplaceMatrix();
-    this->M = geometry->massMatrix();
-    this->totalArea = 0;                 // placeholder
+ScalarPoissonProblem::ScalarPoissonProblem(ManifoldSurfaceMesh* inputMesh,
+                                           VertexPositionGeometry* inputGeo) {
+  mesh = inputMesh;
+  geometry = inputGeo;
+  this->A = geometry->laplaceMatrix();
+  this->M = geometry->massMatrix();
+  this->totalArea = geometry->totalArea();
 }
 
 /*
- * Computes the solution of the poisson problem Ax = -M(rho - rhoBar), where A is the POSITIVE DEFINITE Laplace matrix
- * and M is the mass matrix.
+ * Computes the solution of the poisson problem Ax = -M(rho - rhoBar), where A
+ * is the POSITIVE DEFINITE Laplace matrix and M is the mass matrix.
  *
  * Input: <rho>, the density of vertices in the mesh.
  * Returns: The solution vector.
  */
 Vector<double> ScalarPoissonProblem::solve(const Vector<double>& rho) const {
-
-    // TODO
-    // Note: Geometry Central has linear solvers: https://geometry-central.net/numerical/linear_solvers/
-    return Vector<double>::Zero(rho.rows()); // placeholder
+  // TODO
+  // Note: Geometry Central has linear solvers:
+  // https://geometry-central.net/numerical/linear_solvers/
+  return Vector<double>::Zero(rho.rows());  // placeholder
 }
