030b3c30b752c3586c1125e2be27f3f3f21a4b5b
Alireza <alirezafa@rocketmail.com>
2021-06-25 18:34:21 +0430

    PoissonProblem solved!

--- projects/poisson-problem/src/scalar-poisson-problem.cpp
+++ projects/poisson-problem/src/scalar-poisson-problem.cpp
@@ -1,6 +1,8 @@
 // Implement member functions for ScalarPoissonProblem class.
 #include "scalar-poisson-problem.h"
 
+#include "geometrycentral/numerical/linear_solvers.h"
+
 /* Constructor
  * Input: The surface mesh <inputMesh> and geometry <inputGeo>.
  */
@@ -21,8 +23,14 @@ ScalarPoissonProblem::ScalarPoissonProblem(ManifoldSurfaceMesh* inputMesh,
  * Returns: The solution vector.
  */
 Vector<double> ScalarPoissonProblem::solve(const Vector<double>& rho) const {
-  // TODO
   // Note: Geometry Central has linear solvers:
   // https://geometry-central.net/numerical/linear_solvers/
-  return Vector<double>::Zero(rho.rows());  // placeholder
+  // Page 101 of the Notes
+  // rhoBar = (1/totalArea) * \int rho dV = (1/totalArea) * M * rho
+  auto rhoBar = Vector<double>::Constant(
+      rho.size(), (this->M * rho).sum() / this->totalArea);
+  SparseMatrix<double> laplace{this->A};
+  Vector<double> rhs = -this->M * (rho - rhoBar);
+  // Solve Ax = -M(rho - rhoBar)
+  return solvePositiveDefinite(laplace, rhs);
 }
