3798ca01650de3852611c1bb637a0d356a13249e
Alireza <alirezafa@rocketmail.com>
2021-07-03 12:04:08 +0430

    Implemented Modified Mean Curvature Flow.

--- projects/geometric-flow/src/modified-mean-curvature-flow.cpp
+++ projects/geometric-flow/src/modified-mean-curvature-flow.cpp
@@ -4,14 +4,12 @@
 /* Constructor
  * Input: The surface mesh <inputMesh> and geometry <inputGeo>.
  */
-ModifiedMeanCurvatureFlow::ModifiedMeanCurvatureFlow(ManifoldSurfaceMesh* inputMesh, VertexPositionGeometry* inputGeo) {
-
-    // Build member variables: mesh, geometry, and A (Laplace matrix)
-    mesh = inputMesh;
-    geometry = inputGeo;
-
-    // TODO: build the Laplace matrix
-    this->A = identityMatrix<double>(1); // placeholder
+ModifiedMeanCurvatureFlow::ModifiedMeanCurvatureFlow(
+    ManifoldSurfaceMesh* inputMesh, VertexPositionGeometry* inputGeo) {
+  // Build member variables: mesh, geometry, and A (Laplace matrix)
+  mesh = inputMesh;
+  geometry = inputGeo;
+  this->A = geometry->laplaceMatrix();
 }
 
 /*
@@ -20,7 +18,9 @@ ModifiedMeanCurvatureFlow::ModifiedMeanCurvatureFlow(ManifoldSurfaceMesh* inputM
  * Input: The mass matrix <M> of the mesh, and the timestep <h>.
  * Returns: A sparse matrix representing the mean curvature flow operator.
  */
-SparseMatrix<double> ModifiedMeanCurvatureFlow::buildFlowOperator(const SparseMatrix<double>& M, double h) const {
-    // TODO
-    return identityMatrix<double>(1); // placeholder
+SparseMatrix<double> ModifiedMeanCurvatureFlow::buildFlowOperator(
+    const SparseMatrix<double>& M, double h) const {
+  auto const vertexCount = mesh->nVertices();
+  auto identity = identityMatrix<double>(vertexCount);
+  return M + h * this->A;
 }
